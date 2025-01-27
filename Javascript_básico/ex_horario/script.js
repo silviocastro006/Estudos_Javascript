50acc06fadfc665e64d188bcfdac810f8fafbc81
Sílvio Ricardo <silviocastro006@gmail.com>
2025-01-27 20:44:52 -0300

    coisas

---
+++ Javascript_básico/ex_horario/script.js
@@ -0,0 +1,13 @@
+function carregar(){
+    var msg = window.document.getElementById('msg')
+    var img = window.document.getElementById('img')
+    var data = new Date()
+    var hora = data.getHours()
+    var minutos = data.getMinutes()
+    msg.innerHTML = `Agora são ${hora}:${minutos} h`
+
+    if
+
+}
+
+
